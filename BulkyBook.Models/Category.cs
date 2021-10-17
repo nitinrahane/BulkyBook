7daea4c83ab566df18fb2e1a9ef789772a762a28
nitinrahane11 <nitin.rahane11@gmail.com>
2021-10-18 00:58:16 +0530

    Added Category List related change & repository pattern to the solution

---
+++ BulkyBook.Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name="Category Name")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
